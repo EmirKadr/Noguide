16c4ffebd8510e39e1b9a1680649064d008851f9
EmirKadr <emir.k@nowaste.se>
2025-08-06 12:58:03 +0200

    Update app4.1.py

--- app4.1.py
+++ app4.1.py
@@ -241,4 +241,4 @@ with gr.Blocks() as demo:
         query2.change(fn=search_word_doc, inputs=query2, outputs=output2)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
